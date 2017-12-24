34bee07e82b40cbc34212d6d3f3a14c6f6c0348b
casid <andy@mazebert.com>
2017-12-24 15:01:28 +0100

    Implemenent InputPlugin and ImageCapturePlugin

---
+++ src/main/java/org/jusecase/poe/plugins/RobotPlugin.java
@@ -0,0 +1,32 @@
+package org.jusecase.poe.plugins;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+
+public class RobotPlugin implements InputPlugin, ImageCapturePlugin {
+    private final Robot robot;
+
+    public RobotPlugin() {
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void clickWithControlPressed(int x, int y) {
+        robot.mouseMove(x, y);
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.mousePress(InputEvent.BUTTON1_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_MASK);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+    }
+
+    @Override
+    public BufferedImage captureScreen(Rectangle area) {
+        return robot.createScreenCapture(area);
+    }
+}
