2bd6206896006d9d22890ba50ee1abd406e238f2
casid <andy@mazebert.com>
2017-12-26 13:57:32 +0100

    Test that there are no item conflicts

--- src/test/java/org/jusecase/poe/gateways/ResourceItemGatewayTest.java
+++ src/test/java/org/jusecase/poe/gateways/ResourceItemGatewayTest.java
@@ -14,9 +14,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ResourceItemGatewayTest implements ComponentTest {
     ResourceItemGateway gateway;
 
+    private ImageHashPlugin imageHashPlugin;
+
     @BeforeEach
     void setUp() {
-        givenDependency(new ImageHashPlugin());
+        givenDependency(imageHashPlugin = new ImageHashPlugin());
         gateway = new ResourceItemGateway();
     }
 
@@ -30,5 +32,16 @@ class ResourceItemGatewayTest implements ComponentTest {
         assertThat(currencies.get(51).type).isEqualTo(ItemType.CARD);
         assertThat(currencies.get(51 + 1).type).isEqualTo(ItemType.ESSENCE);
         assertThat(currencies.get(51 + 1 + 104).type).isEqualTo(ItemType.MAP);
+    }
+
+    @Test
+    void noItemConflicts() {
+        for (Item item : gateway.getAll()) {
+            for (Item otherItem : gateway.getAll()) {
+                if (item.type != otherItem.type) {
+                    assertThat(imageHashPlugin.isSimilar(item.imageHash, otherItem.imageHash)).describedAs("expecting " + item + " not to be similar to item " + otherItem).isFalse();
+                }
+            }
+        }
     }
 }
